b56881c30fa2c26fc0c49b52911a5d55c91c2234
Kawisara Bunyen <zelotype.3@gmail.com>
2019-10-06 18:50:02 +0700

    Complete P06 / P06Test

--- src/main/scala/P06.scala
+++ src/main/scala/P06.scala
@@ -1,3 +1,6 @@
 object P06 {
-
+  def checkPalindromeList[A](ls:List[A]):Boolean = {
+    val rvList = ls.reverse
+    rvList.equals(ls)
+  }
 }
--- src/test/scala/P06Test.scala
+++ src/test/scala/P06Test.scala
@@ -1,3 +1,7 @@
-class P06Test {
+import org.scalatest.{FunSuite, Matchers}
 
+class P06Test extends FunSuite with Matchers{
+  test("return true when input palindrome list") {
+    P06.checkPalindromeList(List(1,2,3,3,2,1)) shouldEqual true
+  }
 }
