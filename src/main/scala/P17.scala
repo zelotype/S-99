8a08f8d3c856fad472f7c3fb0a5de7dd5410868b
Kawisara Bunyen <zelotype.3@gmail.com>
2019-10-09 00:35:49 +0700

    P17 done

--- src/main/scala/P17.scala
+++ src/main/scala/P17.scala
@@ -1,9 +1,11 @@
 object P17 {
-//  def splitList[A](length:Int, ls:List[A]):List[A] = {
-//    def splitRecursive[A](count:Int, curLs:List[A], result: List[A]):List[A] = (length, curLs) match {
-//      case (1, h::tail) =>
-//    }
-//    splitRecursive(length, ls, Nil)
-//  }
+  def splitList[A](length:Int, ls:List[A]):(List[A], List[A]) = {
+    def splitRecursive[A](count:Int, curLs:List[A], result: List[A]):(List[A], List[A]) = (count, curLs) match {
+      case (0, tail) => (result.reverse, tail)
+      case (_, h::tail) => splitRecursive(count-1, tail, h::result)
+      case (_ ,  Nil) => (result.reverse, Nil)
+    }
+    splitRecursive(length, ls, Nil)
+  }
   def splitListBuiltIn[A](length:Int, ls:List[A]):(List[A], List[A]) = ls.splitAt(length)
 }
--- src/test/scala/P17Test.scala
+++ src/test/scala/P17Test.scala
@@ -4,4 +4,7 @@ class P17Test extends FunSuite with Matchers{
   test("return 2 list that split from the length was given") {
     P17.splitListBuiltIn(3, List('a, 'b, 'c, 'd, 'e, 'f, 'g, 'h, 'i, 'j, 'k)) shouldEqual (List('a, 'b, 'c),List('d, 'e, 'f, 'g, 'h, 'i, 'j, 'k))
   }
+  test("return 2 list that split from the length was given -- version 2") {
+    P17.splitList(3, List('a, 'b, 'c, 'd, 'e, 'f, 'g, 'h, 'i, 'j, 'k)) shouldEqual (List('a, 'b, 'c),List('d, 'e, 'f, 'g, 'h, 'i, 'j, 'k))
+  }
 }
